095654261311c2dea9a5e635b9aad9d2a00645c5
Marcosfsalazar <Marcos1010521@gmail.com>
2020-12-07 17:14:54 -0300

    Adicionar arquivos de projeto.

---
+++ AspNetCore_MVC_01/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore_MVC_01.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AspNetMvc01;Integrated Security=True");
+        }
+    }
+}
